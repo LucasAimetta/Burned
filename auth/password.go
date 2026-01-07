548b3607bba77ba5d5c20fcfcedff61763f3b979
LucasAimetta <166944360+LucasAimetta@users.noreply.github.com>
2026-01-07 18:22:30 -0300

    feat(auth): Creacion de metodos de autenticacion

---
+++ auth/password.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+func ValidatePassword(password string) bool {
+	var hasUpper, hasLower, hasNumber bool
+	if len(password) < 12 {
+		return false
+	}
+	for _, c := range password {
+		switch {
+		case unicode.IsUpper(c):
+			hasUpper = true
+		case unicode.IsLower(c):
+			hasLower = true
+		case unicode.IsDigit(c):
+			hasNumber = true
+		}
+	}
+	return hasUpper && hasLower && hasNumber
+}
